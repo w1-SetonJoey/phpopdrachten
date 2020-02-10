6c151361c1e5bc7eea9afd2b54b7228d45a9dcab
Joey <joey.seton@edu-kw1c.nl>
2020-02-10 15:53:40 +0100

    Ik heb opdracht 2.2 hier afgemaakt.

--- Periode 3/Hoofdstuk 2.2/opdracht22.php
+++ Periode 3/Hoofdstuk 2.2/opdracht22.php
@@ -1,1 +1,30 @@
 <?php
+/**
+ * User: Joey Seton
+ * Date: 10/02/2020
+ * Time: 14:50 PM
+ * File: opdracht22.php
+ */
+$text1 = "Hallo";
+$text2 = " een makkelijke taal";
+$text3 = " toch zo'n makkelijke taal";
+$text4 = " wat is";
+$text5 = " PHP";
+$text6 = " Nooit gedacht dat";
+$text7 = " De installatie is best ingewikkeld";
+$text8 = " Fijn";
+$text9 = "?";
+$text10 = ".";
+$text11 = ",";
+$text12 = "<br>";
+$text13 = " is";
+$text14 = " Vind je niet";
+$text15 = " toch";
+$text16 = " dat";
+$text17 = " zo'n makkelijke taal";
+
+
+echo "<p>".$text1.$text11.$text4.$text5.$text15.$text2.$text10.$text12.$text7.$text10.$text8.$text15.$text9.$text12.$text6.$text5.$text3.$text13.$text10."</p>";
+
+echo "<p>".$text1.$text11.$text12.$text8.$text15.$text16.$text5.$text17.$text13.$text10.$text12.$text7.$text10.$text14.$text9."</p>";
+?>
