fdd35357d4394c7156f9914091c94b181ce03a2c
Joey <joey.seton@edu-kw1c.nl>
2020-02-10 15:59:11 +0100

    Ik heb hier de index aangepast

--- Periode 3/index.php
+++ Periode 3/index.php
@@ -22,7 +22,7 @@ echo "Hello world";
                     <a href="hoofdstuk2/opdracht_2-1.php">Opdracht 2.1</a>
                 </li>
                 <li>
-                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                    <a href="Hoofdstuk2.2/opdracht22.php">Opdracht 2.2</a>
                 </li>
             </ul>
         </li>
